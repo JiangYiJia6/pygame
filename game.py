57c8c6d493b509f920f93e4d87cc460f7827a761
JiangYiJia6 <116914241+JiangYiJia6@users.noreply.github.com>
2023-07-04 21:45:34 -0600

    first commit

--- game.py
+++ game.py
@@ -1,1 +1,47 @@
 import pygame
+import os
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("game")
+clock = pygame.time.Clock()
+
+root_dir = os.path.dirname(__file__) + "/"
+
+test_font = pygame.font.Font(root_dir +"Pixeltype.ttf", 50)
+test_surface = pygame.Surface((100,200))
+test_surface.fill("red")
+
+sky_surface = pygame.image.load(root_dir + "sky.png").convert()
+
+ground_surface = pygame.image.load(root_dir + "ground.png").convert()
+
+text_surface = test_font.render("My Game", False, "white")
+
+snail_surface = pygame.image.load(root_dir + "snail.png").convert_alpha()
+snail_rect = snail_surface.get_rect(bottomright=(600,300))
+
+player_surface = pygame.image.load(root_dir + "player_walk_1.png").convert_alpha()
+player_rect = player_surface.get_rect(midbottom=(80,300))
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(test_surface, (200,100))
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface, (300,50))
+    
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
+    
+    #draw elements & update everything
+    pygame.display.update()
+    clock.tick(60)

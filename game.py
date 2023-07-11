889230e79cd336561dc759ea5bc5794d2fc32d84
JiangYiJia6 <116914241+JiangYiJia6@users.noreply.github.com>
2023-07-11 18:02:47 -0400

    ;)

--- game.py
+++ game.py
@@ -17,7 +17,8 @@ sky_surface = pygame.image.load(root_dir + "sky.png").convert()
 
 ground_surface = pygame.image.load(root_dir + "ground.png").convert()
 
-text_surface = test_font.render("My Game", False, "white")
+score_surface = test_font.render("My Game", False, "white")
+score_rect = score_surface.get_rect(center=(400,50))
 
 snail_surface = pygame.image.load(root_dir + "snail.png").convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright=(600,300))
@@ -30,11 +31,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type ==pygame.MOUSEMOTION:
+            print(event.pos)
 
     screen.blit(test_surface, (200,100))
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (300,50))
+    pygame.draw.rect()
+    screen.blit(score_surface, (300,50))
     
     snail_rect.x -= 4
     if snail_rect.right <= 0:
@@ -42,6 +46,10 @@ while True:
     screen.blit(snail_surface, snail_rect)
     screen.blit(player_surface, player_rect)
     
+    mouse_pos = pygame.mouse.get_pos()
+    if player_rect.collidepoint( mouse_pos):
+        print("collision")
+
     #draw elements & update everything
     pygame.display.update()
     clock.tick(60)
